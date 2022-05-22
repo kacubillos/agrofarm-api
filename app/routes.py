f26e9dda31a803d2c5b6906338ef04dfecf68f39
Kevin Cubillos Pinzón <kevinlexr@gmail.com>
2022-05-22 15:25:44 -0500

    Login y creacion de tokens para proteccion de rutas

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,8 @@
+from functools import wraps
+
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from flask_jwt_extended import get_jwt, verify_jwt_in_request
 
 from app.models import Admin, Article, Farmer, Crop
 from app.schemas import AdminSchema, ArticleSchema, FarmerSchema, CropSchema
@@ -18,6 +22,49 @@ def page_not_found(error):
 def index():
     return 'Hola mundo desde views'
 
+""" AUTH ROUTES """
+
+def admin_required():
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if claims["is_administrator"]:
+                return fn(*args, **kwargs)
+            else:
+                return jsonify(msg="Admins only!"), 403
+
+        return decorator
+
+    return wrapper
+
+@page.route('/api/admins/login', methods=['POST'])
+def admin_login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = Admin.query.filter(Admin.email == email).first()
+    if not user or not user.verify_password(password):
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    additional_claims = {"is_administrator": True}
+    access_token = create_access_token(identity=email, additional_claims=additional_claims)
+    return jsonify(access_token=access_token)
+
+@page.route('/api/auth/login', methods=['POST'])
+def farmer_login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = Farmer.query.filter(Farmer.email == email).first()
+    if not user or not user.verify_password(password):
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    additional_claims = {"is_administrator": False}
+    access_token = create_access_token(identity=email, additional_claims=additional_claims)
+    return jsonify(access_token=access_token)
+
 """ ADMINS ROUTES """
 
 @page.route('/api/admins/', methods=['GET', 'POST'])
@@ -27,7 +74,7 @@ def admins():
         email = request.json['email']
         password = request.json['password']
 
-        admin = Admin.create_element(name, email, password)
+        admin = Admin.create_element(name, email.lower(), password)
         return admin_schema.jsonify(admin), 201
 
     all_admins = Admin.query.all()
@@ -61,7 +108,7 @@ def farmers():
         email = request.json['email']
         password = request.json['password']
 
-        farmer = Farmer.create_element(name, email, password)
+        farmer = Farmer.create_element(name, email.lower(), password)
         return farmer_schema.jsonify(farmer), 201
 
     all_farmers = Farmer.query.all()
@@ -89,17 +136,23 @@ def delete_farmer(farmer_id):
 """ ARTICLES ROUTES """
 
 @page.route('/api/articles/')
+@jwt_required()
+@admin_required()
 def get_articles():
     all_articles = Article.query.all()
     result = article_schema.dump(all_articles, many=True)
     return jsonify(result)
 
 @page.route('/api/articles/<int:article_id>')
+@jwt_required()
+@admin_required()
 def get_article(article_id):
     article = Article.query.get_or_404(article_id)
     return article_schema.jsonify(article)
 
 @page.route('/api/articles/', methods=['POST'])
+@jwt_required()
+@admin_required()
 def create_article():
     title = request.json['title']
     content = request.json['content']
@@ -111,6 +164,8 @@ def create_article():
     return article_schema.jsonify(article), 201
 
 @page.route('/api/articles/<int:article_id>', methods=['PUT'])
+@jwt_required()
+@admin_required()
 def update_article(article_id):
     title = request.json['title']
     content = request.json['content']
@@ -122,24 +177,29 @@ def update_article(article_id):
     return article_schema.jsonify(article)
 
 @page.route('/api/articles/<int:article_id>', methods=['DELETE'])
+@jwt_required()
+@admin_required()
 def delete_article(article_id):
     article = Article.delete_article(article_id)
     return article_schema.jsonify(article)
 
-""" ARTICLES ROUTES """
+""" CROPS ROUTES """
 
 @page.route('/api/crops/')
+@jwt_required()
 def get_crops():
     all_crops = Crop.query.all()
     result = crop_schema.dump(all_crops, many=True)
     return jsonify(result)
 
 @page.route('/api/crops/<int:crop_id>')
+@jwt_required()
 def get_crop(crop_id):
     crop = Crop.query.get_or_404(crop_id)
     return crop_schema.jsonify(crop)
 
 @page.route('/api/crops/', methods=['POST'])
+@jwt_required()
 def create_crop():
     title = request.json['title']
     crop_type = request.json['crop_type']
@@ -150,6 +210,7 @@ def create_crop():
     return crop_schema.jsonify(crop), 201
 
 @page.route('/api/crops/<int:crop_id>', methods=['PUT'])
+@jwt_required()
 def update_crop(crop_id):
     title = request.json['title']
     crop_type = request.json['crop_type']
@@ -160,6 +221,7 @@ def update_crop(crop_id):
     return crop_schema.jsonify(crop)
 
 @page.route('/api/crops/<int:crop_id>', methods=['DELETE'])
+@jwt_required()
 def delete_crop(crop_id):
     crop = Crop.delete_crop(crop_id)
     return crop_schema.jsonify(crop)
